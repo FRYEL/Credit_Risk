67cfce44a5f2287168030bcc7bb0179d362fcbff
Furkan Yel <furkanyel20@gmail.com>
2024-02-28 11:03:38 +0100

    fix preprocessing

--- main.py
+++ main.py
@@ -1,20 +1,82 @@
-# This is a sample Python script.
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.ticker import ScalarFormatter
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+data = pd.read_csv("data/cleaned_data.csv")
 
-data = pd.read_csv("data/loan.csv")
+# Histogram
+plt.figure(figsize=(10, 6))
+plt.hist(data['loan_amnt'], bins=30, color='skyblue', edgecolor='black')
+plt.xlabel('Loan Amount')
+plt.ylabel('Frequency')
+plt.title('Distribution of Loan Amounts')
+plt.show()
 
-print(data.columns)
+# Bar Chart
+plt.figure(figsize=(10, 6))
+data['term'].value_counts().sort_index().plot(kind='bar')
+plt.xlabel('Loan Term')
+plt.ylabel('Count')
+plt.title('Distribution of Loan Terms')
+plt.show()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Pie Chart
+plt.figure(figsize=(8, 8))
+
+grade_counts = data['grade'].value_counts()
+grade_order = sorted(grade_counts.index)
+grade_counts.loc[grade_order].plot(kind='pie', autopct='%1.1f%%', colors=sns.color_palette('pastel'))
+
+plt.title('Distribution of Loan Grades')
+plt.ylabel('')
+plt.show()
+
+# Boxplot
+plt.figure(figsize=(10, 6))
+sns.boxplot(x=data['grade'], y=data['int_rate'], order=['A', 'B', 'C', 'D', 'E', 'F', 'G'], palette='viridis')
+plt.xlabel('Loan Grade')
+plt.ylabel('Interest Rate')
+plt.title('Interest Rate Distribution by Loan Grade')
+plt.show()
+
+# Scatterplot
+
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x=data['loan_amnt'], y=data['annual_inc'], alpha=0.5)
+plt.xlabel('Loan Amount')
+plt.ylabel('Annual Income')
+plt.title('Loan Amount vs. Annual Income')
+plt.yscale('log')
+
+median_annual_inc = data['annual_inc'].median()
+plt.axhline(y=median_annual_inc, color='r', linestyle='--', label='Median Annual Income')
+
+formatter = ScalarFormatter()
+formatter.set_scientific(False)
+plt.gca().yaxis.set_major_formatter(formatter)
+
+plt.legend()
+plt.show()
+
+# Correlation Heatmap
+plt.figure(figsize=(12, 8))
+corr = data.corr()
+sns.heatmap(corr, annot=True, cmap='inferno', fmt=".2f")
+plt.title('Correlation Heatmap')
+plt.show()
+
+# Stacked Bar Chart
+plt.figure(figsize=(10, 6))
+stacked_df_indv = data.groupby(['term', 'grade']).size().unstack()
+stacked_df_indv.plot(kind='bar', stacked=True)
+plt.xlabel('Loan Term')
+plt.ylabel('Count of Loans')
+plt.title('Distribution of Loan Grades by Term')
+plt.legend(title='Loan Grade')
+plt.show()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+ pass
