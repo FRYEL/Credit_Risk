1ae21fc56897412c48278f71b8fc0ce8d8ec5c7f
Furkan Yel <furkanyel20@gmail.com>
2024-03-16 22:02:59 +0100

    new way to unzip

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import sys
 
-df = pd.read_csv("data/cleaned_data.csv", low_memory = False)
+# Check for python version greater 2.8
+if sys.version_info[0:2] < (2, 8):
+    raise Exception("Requires python 2.8 or higher")
+
+df = pd.read_csv("data/cleaned_data.csv", low_memory=False)
 
 if __name__ == '__main__':
- pass
+    pass
