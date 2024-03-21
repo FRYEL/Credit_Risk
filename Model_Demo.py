34c4029b415dfb7d118c5be88d15ec7863e07de2
Furkan Yel <furkanyel20@gmail.com>
2024-03-21 14:35:50 +0100

    rm old run project

--- Model_Demo.py
+++ Model_Demo.py
@@ -1,6 +1,5 @@
 import mlflow
 import pandas as pd
-import numpy as np
 from sklearn.metrics import accuracy_score, f1_score, roc_curve, classification_report, precision_recall_curve
 from utils.log import LOGGER
 import matplotlib.pyplot as plt
