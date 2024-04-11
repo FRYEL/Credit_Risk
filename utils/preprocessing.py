7ede82835df931fc495dcd08147711dd9a4915b5
Furkan Yel <furkanyel20@gmail.com>
2024-04-11 18:51:01 +0200

    add 20% initial split for demo

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,6 +2,8 @@
 preprocessing.py Script to preprocess the data
 """
 import numpy as np
+from sklearn.model_selection import train_test_split
+import pandas as pd
 from utils.log import LOGGER
 import category_encoders as ce
 import warnings
@@ -175,7 +177,18 @@ def save_preprocessed_data(df, filepath="../data/cleaned_data.csv"):
     :param filepath: filepath to save
     :return: None
     """
-    df.to_csv(filepath, index=False)
+    X_train, X_test, y_train, y_test = train_test_split(df,
+                                                        df['loan_status'],
+                                                        test_size=0.2,
+                                                        random_state=77)
+
+    # Merging Demo Dataset for later
+    demo_df = pd.concat([X_test, y_test], axis=1)
+    demo_df.to_csv("../data/demo_data.csv", index=False)
+
+    # Merging back into one DataFrame
+    reduced_df = pd.concat([X_train, y_train], axis=1)
+    reduced_df.to_csv(filepath, index=False)
 
 
 def preprocessing_pipeline(df):
@@ -194,4 +207,6 @@ def preprocessing_pipeline(df):
     df = format_column_order(df)
     save_preprocessed_data(df)
     LOGGER.info("Preprocessing finished!")
+
+
     return df
